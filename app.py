3655483e4a7cc485630494c3aa5123f9ccecd6db
tris <tris.bouden@gmail.com>
2024-11-06 17:46:46 +0100

    add mask word prediction logic

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import gradio as gr
+from mask import mask_word_prediction
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+def greet(text):
+    result = mask_word_prediction(text, 1)
+    return result[0]
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=gr.Textbox(label="Enter a sentence with a masked word ([MASK]):"),
+    outputs="text",
+)
 demo.launch()
---
+++ mask.py
@@ -0,0 +1,35 @@
+import string
+import tensorflow as tf
+from transformers import AutoTokenizer, TFBertForMaskedLM
+
+MODEL = "bert-base-uncased"
+
+
+def mask_word_prediction(text, K=5):
+    results = []
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    inputs = tokenizer(text, return_tensors="tf")
+    mask_token_index = get_mask_token_index(tokenizer.mask_token_id, inputs)
+    if mask_token_index is None:
+        print(f"Input must include mask token {tokenizer.mask_token}.")
+        return
+
+    model = TFBertForMaskedLM.from_pretrained(MODEL)
+    mask_token_logits = model(**inputs).logits[0, mask_token_index]
+    top_tokens = tf.math.top_k(mask_token_logits, K).indices.numpy()
+    filtered_tokens = [
+        token
+        for token in top_tokens
+        if tokenizer.decode([token]) not in string.punctuation
+    ]
+    for token in filtered_tokens:
+        result = text.replace(tokenizer.mask_token, tokenizer.decode([token]))
+        results.append(result)
+
+    return results
+
+
+def get_mask_token_index(mask_token_id, inputs):
+    token_ids = inputs["input_ids"][0].numpy()
+    return list(token_ids).index(mask_token_id) if mask_token_id in token_ids else None
